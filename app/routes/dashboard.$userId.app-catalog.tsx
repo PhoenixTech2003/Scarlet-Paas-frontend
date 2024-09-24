88c1bcd7e87c3c92a772bb04ff78fb8a833fb64e
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-24 21:37:46 +0200

    Add AppCatalogCard component to display app catalog items

--- app/routes/dashboard.$userId.app-catalog.tsx
+++ app/routes/dashboard.$userId.app-catalog.tsx
@@ -1,9 +1,21 @@
+import AppCatalogCard from "~/components/app-catalog-card";
+import { TbBrandVite } from "react-icons/tb";
+
 export default function AppCatalog() {
+  const iconStyling= {size:50};
+  const apps = [{ name: "React + Vite", icon: <TbBrandVite  size={iconStyling.size} /> }];
   return (
     <div>
       <h1 className="text-xl font-">
         Welcome Chiyembekezo What would you like to deploy today
       </h1>
+      <section className="grid grid-cols-3 mt-14">
+        {apps.map((app) => (
+          <article key={app.name}>
+            <AppCatalogCard details={{ name: app.name, icon: app.icon }} />
+          </article>
+        ))}
+      </section>
     </div>
   );
 }
