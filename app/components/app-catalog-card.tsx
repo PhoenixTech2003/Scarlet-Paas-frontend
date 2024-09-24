4cdb01e97a002d60bd5a3b0b9831bbddc41e9b27
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-24 21:37:14 +0200

    Add AppCatalogCard component

---
+++ app/components/app-catalog-card.tsx
@@ -0,0 +1,41 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { FaArrowRight } from "react-icons/fa";
+import type { AppCatalogCard } from "~/lib/definitions";
+
+export default function AppCatalogCard({
+  details,
+}: {
+  details: AppCatalogCard;
+}) {
+  return (
+    <Dialog>
+      <DialogTrigger className=" rounded-md bg-gradient-to-r grid gap-4 from-slate-900 to-slate-800 py-24 px-4">
+        <div>
+
+       <p className="flex justify-center">{details.icon}</p> 
+        <p>{details.name}</p>
+        </div>
+        <div className="flex items-center gap-2 self-center">
+          <p className="font-normal text-lg">Click to start Deployment</p>
+          <FaArrowRight color="#e11d48" />
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
