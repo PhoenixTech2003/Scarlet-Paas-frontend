ff7a901a1d34019d1e874925169f1e8639d11071
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-25 13:44:04 +0200

    Refactor root.tsx and add Clerk authentication

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,7 +1,11 @@
 import NavLinks from "~/components/navigation-links";
 import { Outlet } from "@remix-run/react";
+import { SignedIn, UserButton} from "@clerk/remix";
+
+
 export default function Dashboard() {
   return (
+    <SignedIn>
     <main className="h-dvh grid grid-cols-[0.4fr_2fr]">
       <section className="bg-gradient-to-r from-slate-900 to-slate-800 text-white p-8">
         <div>
@@ -10,13 +14,21 @@ export default function Dashboard() {
           </p>
 
           <section className="mt-8">
+            
             <NavLinks />
           </section>
         </div>
       </section>
-      <section className="text-white font-bold text-2xl p-6 text-center overflow-y-scroll">
+      <section className="text-white font-bold text-2xl p-6 text-center overflow-y-scroll"> 
+        <div className="flex justify-end">
+          <UserButton/>
+          </div>     
         <Outlet />
       </section>
     </main>
+
+
+
+    </SignedIn>
   );
 }
