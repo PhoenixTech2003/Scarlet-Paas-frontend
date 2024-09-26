4122eee8c012c7b06be4ef5dc764f365583938a2
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-26 21:40:55 +0200

    Add real-time logging functionality using socket.io-client

--- app/routes/dashboard.$userId.deployments.tsx
+++ app/routes/dashboard.$userId.deployments.tsx
@@ -1,3 +1,9 @@
+import { socket } from "~/sockets/sockets";
+import { useEffect } from "react";
+
 export default function Deployments() {
+  useEffect(()=>{
+    socket.on('log', (data) => console.log(data) )
+  })
   return <div className="text-white">hello from the deployments</div>;
 }
