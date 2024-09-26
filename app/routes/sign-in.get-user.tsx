da8edba6af134ebd36e3d3b34c265c433b514ab3
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-26 09:01:41 +0200

    Refactor sign-in route to use userId directly for redirect

--- app/routes/sign-in.get-user.tsx
+++ app/routes/sign-in.get-user.tsx
@@ -1,18 +1,10 @@
 
 import { redirect, LoaderFunction } from "@remix-run/node";
 import { getAuth } from '@clerk/remix/ssr.server'
-import { createClerkClient } from "@clerk/remix/api.server";
-import { getMongoUserId } from "~/lib/data";
 
 
 export const loader:LoaderFunction = async(args)=>{
     const {userId} = await getAuth(args)
-    const user = await createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY }).users.getUser(
-        userId as string,
-      )
-      const email = user.emailAddresses[0].emailAddress
-      const mongoUserId = (await getMongoUserId(email)).data.userId
-      
-    return redirect(`/dashboard/${mongoUserId}/app-catalog`)
+    return redirect(`/dashboard/${userId}/app-catalog`)
 
 }
