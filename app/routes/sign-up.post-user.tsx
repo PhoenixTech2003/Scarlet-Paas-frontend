e0a44b2d8deb8be7dff01d8f135043ae548f2d86
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-26 09:02:00 +0200

    Refactor sign-up route to redirect to the correct user's app catalog

--- app/routes/sign-up.post-user.tsx
+++ app/routes/sign-up.post-user.tsx
@@ -1,21 +1,10 @@
 import { redirect, LoaderFunction } from "@remix-run/node";
 import { getAuth } from '@clerk/remix/ssr.server'
-import { createClerkClient } from "@clerk/remix/api.server";
-import { saveUserDataToMongo, getMongoUserId } from "~/lib/data";
+
 
 
 export const loader:LoaderFunction = async(args)=>{
     const {userId} = await getAuth(args)
-    const user = await createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY }).users.getUser(
-        userId as string,
-      )
-
-    const emailAddress = user.emailAddresses[0].emailAddress
-    const firstName = user.firstName as string
-    const lastName = user.lastName as string
-    const id = userId as string
-    await saveUserDataToMongo(id,emailAddress,firstName,lastName)
-    const mongoUserId = (await getMongoUserId(emailAddress)).data.userId
-    return redirect(`/dashboard/${mongoUserId}/app-catalog`)
+    return redirect(`/dashboard/${userId}/app-catalog`)
 
 }
