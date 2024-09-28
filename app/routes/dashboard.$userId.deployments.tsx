79c9c7382bd2ae286f05555404a8dbd50e26bcc3
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-28 11:14:32 +0200

    Refactor dashboard.$userId.deployments.tsx: Remove unused code and add comments

--- app/routes/dashboard.$userId.deployments.tsx
+++ app/routes/dashboard.$userId.deployments.tsx
@@ -1,9 +1,6 @@
-import { socket } from "~/sockets/sockets";
-import { useEffect } from "react";
+
 
 export default function Deployments() {
-  useEffect(()=>{
-    socket.on('log', (data) => console.log(data) )
-  })
+  
   return <div className="text-white">hello from the deployments</div>;
 }
