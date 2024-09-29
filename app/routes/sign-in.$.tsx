bb02e46173994ec3820e098f180041809885d003
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-29 22:25:04 +0200

    Refactor sign-in page: Update UI layout for responsive design

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -1,13 +1,13 @@
 import { SignIn } from "@clerk/remix";
 export default function SignInPage() {
   return (
-    <main className="h-dvh grid grid-cols-[1fr_2fr]">
-      <section className="bg-gradient-to-r from-slate-900 to-slate-800 grid justify-center items-center">
-        <p className="text-white font-extrabold text-4xl">
+    <main className="h-dvh grid md:grid-cols-[1fr_2fr]">
+      <section className="bg-gradient-to-r from-slate-900 to-slate-800 hidden md:grid justify-center items-center">
+        <p className="text-white font-extrabold text-4xl hidden md:block">
           SCARLET<span className="text-rose-600">Deploy</span>
         </p>
       </section>
-      <section className="justify-self-center self-center">
+      <section className="justify-self-center self-center grid justify-center">
         <SignIn />
       </section>
     </main>
